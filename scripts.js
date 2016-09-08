4017ea79aa66e0548082e590f77b269a81bad605
Hilary Lewis <hilarylewis92@gmail.com>
2016-09-07 22:23:29 -0600

    refactor css and delete btn

--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
-// var $title = $('.title-input').val();
-// var $body = $('.body-input').val();
+var title = $('.title-input').val();
+var body = $('.body-input').val();
 var $save = $('.save-btn');
 
 $save.on('click', function() {
   makeIdeaList();
-  JSON
+  // JSON
   clearField($('.title-input'), $('.body-input'));
 });
 
-function makeIdeaList() {
+function makeIdeaList(title, body) {
   return $('.idea-list').append(`
       <li class="idea" id=${Date.now()}>
-        <span>${$('.title-input').val()}</span>
-        <span>${$('.body-input').val()}</span>
+        <span class="title">${$('.title-input').val()}</span>
+        <img class="icon delete-icon" src="./images/delete.svg" alt="delete" /><br>
+        <span>${$('.body-input').val()}</span><br>
+        <img class="icon upvote-icon" src="./images/upvote.svg">
+        <img class="icon downvote-icon" src="./images/downvote.svg">
         <span>quality: </span>
       </li>
   `);
 }
-
-// function JSON
 
 $('.title-input').on('click', function() {
   clearField($('.title-input'));
@@ -32,3 +33,7 @@ function clearField(element1, element2) {
   element1.val('');
   element2.val('');
 }
+
+$('.idea-list').on('click', '.delete-icon', function() {
+  $(this).parent().remove();
+});
