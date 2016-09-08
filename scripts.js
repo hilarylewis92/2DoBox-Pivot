f008d4761f69c5c2d2e4cceb5ff6d469fc7c0b3e
ccgamble <christine.c.gamble@gmail.com>
2016-09-07 22:23:49 -0600

    working on upvote and downvote

--- scripts.js
+++ scripts.js
@@ -1,20 +1,51 @@
 // var $title = $('.title-input').val();
 // var $body = $('.body-input').val();
 var $save = $('.save-btn');
+var counter = 0
+
 
 $save.on('click', function() {
   makeIdeaList();
-  clearField($('.title-input'), $('.body-input'));
+  clearField($('.title-input'));
+  clearField($('.body-input'));
+  downClick()
+  qualityCounter()
 });
+
+function downClick(counter) {
+$('.down-btn').on('click', function() {
+qualityCounter(counter)
+counter = (counter --);
+console.log(counter)
+})
+}
+
+function qualityCounter() {
+  if (counter === 0) {
+    return $('.quality').html("quality:swill");
+  };
+  if (counter ===1) {
+    return $('.quality').html("quality:plausible");
+  };
+  if (counter ===2) {
+    return $('.quality').html("quality:genius");
+  }
+}
 
 function makeIdeaList() {
   return $('.idea-list').append(`
-      <li class="idea">
+      <li class="idea" id= ${Date.now()}>
         <span>${$('.title-input').val()}</span>
         <span>${$('.body-input').val()}</span>
+        <input class="up-btn" type="image" src="icons/upvote.svg">
+        <input class="down-btn" type="image" src="icons/downvote.svg">
+
+        <span class="quality">quality:swill</span>
       </li>
   `);
 }
+
+
 
 $('.title-input').on('click', function() {
   clearField($('.title-input'));
@@ -24,9 +55,8 @@ $('.body-input').on('click', function() {
   clearField($('.body-input'));
 });
 
-function clearField(element1, element2) {
-  element1.val('');
-  element2.val('');
+function clearField(element) {
+  if (element.val !== "") {
+    element.val('');
+  }
 }
-
-function 
