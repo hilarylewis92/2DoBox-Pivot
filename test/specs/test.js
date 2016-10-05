8f474cf8dee265cfa6bbb80d6a318b8454d2cb28
gness1804 <graham.nessler@gmail.com>
2016-10-04 20:05:32 -0600

    makes some syntax improvements

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,31 +2,37 @@ const assert =  require('assert');
 
 describe('attributes on our application',function(){
   it('has input forms and I can set values in those forms', function(){
-    browser.url('/')
-    var todoTitle = browser.element(".title-input")
-    var todoTask = browser.element(".task-input")
+    browser.url('/');
+    var todoTitle = browser.element(".title-input");
+    var todoTask = browser.element(".task-input");
 
-    todoTitle.setValue('great title')
-    todoTask.setValue('great task')
+    todoTitle.setValue('buy milk');
+    todoTask.setValue('buy milk now');
 
-    assert.equal(todoTitle.getValue(), 'great title')
-    assert.equal(todoTask.getValue(), 'great task')
-  })
+    assert.equal(todoTitle.getValue(), 'buy milk');
+    assert.equal(todoTask.getValue(), 'buy milk now');
+  }); //end of set values tuest
 
   it("should be able to add ideas to the page", function () {
     browser.url('/')
-    var todoTitle = browser.element(".title-input")
-    var todoTask = browser.element(".task-input")
-    // var todoImportance = browser.element(".importance-rating");
+    var todoTitle = browser.element(".title-input");
+    var todoTask = browser.element(".task-input");
 
-    todoTitle.setValue('great title')
-    todoTask.setValue('great task')
-    // todoImportance.setValue('High');
+    todoTitle.setValue('buy milk');
+    todoTask.setValue('buy milk now');
 
     browser.click(".save-btn");
 
     var allTodos = browser.getText("li");
 
-     assert.equal(allTodos.replace(/\n/, ", ").replace(/\n/, ", "), 'great title, great task, Importance: Normal')
-  });
+     assert.equal(allTodos.replace(/\n/, ", ").replace(/\n/, ", "), 'buy milk, buy milk now, Importance: Normal')
+  }); //end of add ideas test
+
+  // it("should augment the importance of an idea by one when user presses upvote", function () {
+  //
+  //     var todo = new Todo("Buy milk", "buy milk now");
+  //
+  //
+  //
+  // }); //end of upvote test
 })
